0bdb0d62b5e17d83eb54c0a1b74a3fd2ab80660e
CompareSheriff <comparesheriff@gmx.net>
2022-02-20 20:46:13 +0100

    rename class variable to salary

--- src/payroll/hole01/Payslip.java
+++ src/payroll/hole01/Payslip.java
@@ -1,16 +1,16 @@
 package payroll.hole01;
 
 public class Payslip {
-    private final double sal;
+    private final double salary;
 
     public Payslip(final double slry) {
-        this.sal = slry;
+        this.salary = slry;
     }
 
     public double getNet() {
-        final double ltbg = Math.max(Math.min(sal, 20000.0) - 5000, 0.0);
-        final double mtbg = Math.max(Math.min(sal, 40000) - 20000, 0.0);
-        final double utbg = Math.max(sal - 40000, 0.0);
-        return sal - (ltbg * 0.1 + mtbg * 0.2 + utbg * 0.4);
+        final double ltbg = Math.max(Math.min(salary, 20000.0) - 5000, 0.0);
+        final double mtbg = Math.max(Math.min(salary, 40000) - 20000, 0.0);
+        final double utbg = Math.max(salary - 40000, 0.0);
+        return salary - (ltbg * 0.1 + mtbg * 0.2 + utbg * 0.4);
     }
 }
