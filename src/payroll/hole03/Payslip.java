5b691972e9493210c334ce65178eec81c61c0140
CompareSheriff <comparesheriff@gmx.net>
2022-02-20 20:59:36 +0100

    move instance method to TaxCalculator

--- src/payroll/hole03/Payslip.java
+++ src/payroll/hole03/Payslip.java
@@ -10,13 +10,7 @@ public class Payslip {
     }
 
     public double getNet() {
-        return grossSalary - calculatedTax(grossSalary);
+        return grossSalary - taxCalculator.calculatedTax(grossSalary);
     }
 
-    private double calculatedTax(double grossSalary) {
-        final double lowerTaxBracketGross = Math.max(Math.min(grossSalary, 20000.0) - 5000, 0.0);
-        final double middleTaxBracketGross = Math.max(Math.min(grossSalary, 40000) - 20000, 0.0);
-        final double upperTaxBracketGross = Math.max(grossSalary - 40000, 0.0);
-        return lowerTaxBracketGross * 0.1 + middleTaxBracketGross * 0.2 + upperTaxBracketGross * 0.4;
-    }
 }
