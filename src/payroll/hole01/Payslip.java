106894a0f90ce1e2fdd9b803238aa58799e17df0
CompareSheriff <comparesheriff@gmx.net>
2022-02-20 20:48:07 +0100

    rename method variable to upperTaxBracketGross

--- src/payroll/hole01/Payslip.java
+++ src/payroll/hole01/Payslip.java
@@ -10,7 +10,7 @@ public class Payslip {
     public double getNet() {
         final double lowerTaxBracketGross = Math.max(Math.min(salary, 20000.0) - 5000, 0.0);
         final double middleTaxBracketGross = Math.max(Math.min(salary, 40000) - 20000, 0.0);
-        final double utbg = Math.max(salary - 40000, 0.0);
-        return salary - (lowerTaxBracketGross * 0.1 + middleTaxBracketGross * 0.2 + utbg * 0.4);
+        final double upperTaxBracketGross = Math.max(salary - 40000, 0.0);
+        return salary - (lowerTaxBracketGross * 0.1 + middleTaxBracketGross * 0.2 + upperTaxBracketGross * 0.4);
     }
 }
